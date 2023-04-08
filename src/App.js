53d70d2fc17cd61cbb245964525f757c6e94f1a3
gabypachano <gabrielapachano.13@gmail.com>
2023-04-08 01:58:11 -0300

    'modificaciones en el front'

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ function App() {
          window.alert('Dude, tu numero de ID debe estar entre 1 y 826');
          return;
       }
-      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+      axios(`http://localhost:3001/rickandmorty/character/${id}`)
+      .then(({ data }) => {
          if (data.name && !characters.find((char) => char.id === data.id)) {
             setCharacters((oldChars) => [...oldChars, data]);
          } else {
